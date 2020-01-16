dee883cd1086a4b30f0bb615d85fdc0d2a4d3188
WangFengTu <wangfengtu@huawei.com>
2020-01-16 09:29:16 -0500

    iSulad-kit: Add copyright
    Signed-off-by: WangFengTu <wangfengtu@huawei.com>

--- cmd/isulad_kit/main.go
+++ cmd/isulad_kit/main.go
@@ -1,4 +1,4 @@
-// Copyright (c) Huawei Technologies Co., Ltd. 2019. All rights reserved.
+// Copyright (c) Huawei Technologies Co., Ltd. 2019-2020. All rights reserved.
 // iSulad-kit licensed under the Mulan PSL v1.
 // You can use this software according to the terms and conditions of the Mulan PSL v1.
 // You may obtain a copy of Mulan PSL v1 at:
@@ -10,6 +10,24 @@
 // Description: iSulad image kit
 // Author: lifeng
 // Create: 2019-05-06
+
+// Since some of this code is derived from skopeo, their copyright
+// is retained here....
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// The original version of this file can be found at
+// https://github.com/containers/skopeo/blob/master/cmd/skopeo/main.go
 
 package main
 
