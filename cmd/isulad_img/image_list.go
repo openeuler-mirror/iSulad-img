2483f8d311fe2d8ba94027e7b151839dcabc4905
WangFengTu <wangfengtu@huawei.com>
2020-04-27 00:12:11 -0400

    Change copyright from Mulan PSL v1 to Mulan PSL v2
    Signed-off-by: WangFengTu <wangfengtu@huawei.com>

--- cmd/isulad_img/image_list.go
+++ cmd/isulad_img/image_list.go
@@ -1,12 +1,12 @@
 // Copyright (c) Huawei Technologies Co., Ltd. 2019. All rights reserved.
-// iSulad-img licensed under the Mulan PSL v1.
-// You can use this software according to the terms and conditions of the Mulan PSL v1.
-// You may obtain a copy of Mulan PSL v1 at:
-//     http://license.coscl.org.cn/MulanPSL
+// iSulad-img licensed under the Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//     http://license.coscl.org.cn/MulanPSL2
 // THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
 // PURPOSE.
-// See the Mulan PSL v1 for more details.
+// See the Mulan PSL v2 for more details.
 // Description: iSulad image kit
 // Author: lifeng
 // Create: 2019-05-06
@@ -101,13 +101,13 @@ func listImages(gopts *globalOptions, filter string, check bool) (*listImagesRes
 
 	resp := &listImagesResponse{}
 	for _, result := range results {
-		imageConfig, err := getImageConf(store, result.ID)
-		if err != nil {
-			return nil, err
+		imageConfig, err2 := getImageConf(store, result.ID)
+		if err2 != nil {
+			return nil, err2
 		}
-		healthcheck, err := getHealthcheck(store, result.ID)
-		if err != nil {
-			return nil, err
+		healthcheck, err2 := getHealthcheck(store, result.ID)
+		if err2 != nil {
+			return nil, err2
 		}
 		resImg := &Image{
 			ID:          result.ID,
